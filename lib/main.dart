3cb9c06ede15aedfa1d53e30e6853893eceba866
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-13 21:49:14 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -40,25 +40,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text("TEST APP"),
 
       ),
       body: Center(
         child: Container(width: 400,height: 200,color: Colors.amber,
+
         child: Center(
             child: Text("Hii Bhoiiii",
               style: TextStyle(color: Colors.blueAccent
                   ,fontWeight: FontWeight.bold)
-              ,)),
+              ,),
         ),
+        ),
+
       )
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
