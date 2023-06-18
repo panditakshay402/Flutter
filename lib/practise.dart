ea031bd7254982ff02f2e4b24c0d3d7a8e8e9065
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-18 12:46:43 +0530

    Removed practice.dart

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,22 +1,2 @@
 
 
-import 'package:flutter_1/main.dart';
-
-void main()
-{
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
