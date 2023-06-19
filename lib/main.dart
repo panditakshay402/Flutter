d5900f2ac00599ebdd579260a291db0f04ed16d7
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-19 16:01:20 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'AdsMoney',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.amberAccent),
       home: const MyHomePage(title: 'This is Home Page'),
       debugShowCheckedModeBanner: false,
     );
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.cyanAccent,
         title: Text("TEST APP"),
       ),
       body: Column(
