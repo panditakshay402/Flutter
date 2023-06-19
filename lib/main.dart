f2fb27f1d4689825f32039bd712e8129a3beaa60
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-19 15:39:40 +0530

    Image adding

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,25 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("TEST APP"),
-
       ),
-      body:
-        Image.asset(""),
-
+      body: Column(
+        children: [
+          Image.asset("Images/yori.jpg"),
+          Text(
+            'Yorichi ka First Pic',
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
+          ),
+          Image.network(''),
+          Text(
+            'Yorichi ka Second Pic',
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
+          ),
+        ],
+      ),
     );
   }
 }
