acbb96a4d3aac3f8553ed89694b9cb7f61f2311a
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-20 23:49:28 +0530

    secondpage.dart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
