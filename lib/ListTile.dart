f4949e33070f7eb54066f6b158ab1802dc68ce88
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-23 16:55:39 +0530

    ListView.dart added but Listview coded.

--- lib/ListTile.dart
+++ lib/ListTile.dart
@@ -6,65 +6,26 @@ class ListTiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var arrName = ['akshay', 'Ak', 'AKK', 'AAAA', 'AAyy'];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ListTile"),
-      ),
-      body: Center(
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "One",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "two",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "three",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "four",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "five",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "six",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "seven",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("ListTile"),
         ),
-      ),
-    );
+        body: ListView.separated(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(arrName[index]),
+            );
+          },
+          itemCount: arrName.length,
+          separatorBuilder: (context, index) {
+            return Divider(
+              height: 50,
+              thickness: 2,
+            );
+          },
+        ));
   }
 }
